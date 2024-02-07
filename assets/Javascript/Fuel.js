76914d969cb53b3b4b79995902338a9da6d6a910
Matt Christopherson <matthew.christopherson27@gmail.com>
2024-02-07 13:07:06 -0600

    testing a new API

--- assets/Javascript/Fuel.js
+++ assets/Javascript/Fuel.js
@@ -1,16 +1,12 @@
-const url = 'https://gas-price.p.rapidapi.com/allUsaPrice';
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '66a9e75901mshdccb961c98f7b41p142d8fjsne22a9b9f52c4',
-		'X-RapidAPI-Host': 'gas-price.p.rapidapi.com'
-	}
-};
+let fuelAPiKey = '66a9e75901mshdccb961c98f7b41p142d8fjsne22a9b9f52c4'
+let fuelQueryUrl = 'https://gas-price.p.rapidapi.com/allUsaPrice';
 
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
+function getApi() {
+	fetch(fuelQueryUrl)
+		.then(function (response) {
+			return response.json();
+		})
+		.then (function (data) {
+			console.log(data);
+		})
 }
