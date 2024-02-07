5b6f762e7295a7985ba0a4484370e359f05f5689
Matt Christopherson <matthew.christopherson27@gmail.com>
2024-02-07 14:23:25 -0600

    got the fuel api to work

--- assets/Javascript/Fuel.js
+++ assets/Javascript/Fuel.js
@@ -1,12 +1,41 @@
-let fuelAPiKey = '66a9e75901mshdccb961c98f7b41p142d8fjsne22a9b9f52c4'
-let fuelQueryUrl = 'https://gas-price.p.rapidapi.com/allUsaPrice';
+async function fetchPricesData() {
+    const url = 'https://gas-price.p.rapidapi.com/allUsaPrice';
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '66a9e75901mshdccb961c98f7b41p142d8fjsne22a9b9f52c4',
+            'X-RapidAPI-Host': 'gas-price.p.rapidapi.com'
+        }
+    };
 
-function getApi() {
-	fetch(fuelQueryUrl)
-		.then(function (response) {
-			return response.json();
-		})
-		.then (function (data) {
-			console.log(data);
-		})
+    try {
+        const response = await fetch(url, options);
+        const result = await response.text();
+        console.log(result);
+    } catch (error) {
+        console.error(error);
+    }
 }
+
+fetchPricesData();
+
+async function fetchCityData() {
+	const url = 'https://gas-price.p.rapidapi.com/usaCitiesList';
+	const options = {
+		method: 'GET',
+		headers: {
+			'X-RapidAPI-Key': '66a9e75901mshdccb961c98f7b41p142d8fjsne22a9b9f52c4',
+			'X-RapidAPI-Host': 'gas-price.p.rapidapi.com'
+		}
+	};
+	
+	try {
+		const response = await fetch(url, options);
+		const result = await response.text();
+		console.log(result);
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+fetchCityData();
