4e23608dbc3bb8a21cc6406023b86d6ab8649706
Brenner Jolley <bjbrenardjoey@gmail.com>
2024-02-14 17:15:28 -0700

    Final adjustments

--- assets/Javascript/UDOT.js
+++ assets/Javascript/UDOT.js
@@ -4,51 +4,23 @@ const apiUrl = 'https://www.udottraffic.utah.gov/api/v2/get/cameras';
 
 const requestUrl = `${apiUrl}?key=${apiKey}`;
 const url = 'https://corsproxy.io/?' + encodeURIComponent(requestUrl);
+
 fetch(url)
   .then(response => response.json())
   .then(data => {
     console.log(data);
-var citiesArray = [2, 27, 28, 30, 134, 135, 136, 137, 138, 197, 200, 220, 348, 357, 430, 530];
-    for (var i = 0; i < citiesArray.length; i++) {
+
+    for (var i = 0; i < data.length; i++) {
         
-        var createTableRow = document.createElement("tr");
-        var tableData = document.createElement("td");
-        var link = document.createElement("a");
-        var citiesArrayData = data[citiesArray[i]].Url;
+        var createTableRow = document.createElement('tr');
+        var tableData = document.createElement('td');
+        var link = document.createElement('a');
 
-        link.textContent = data[citiesArray[i]].Url;
-        link.setAttribute("href", citiesArrayData); 
-
-        console.log(citiesArray);
+        link.textContent = data[i].html_url;
+        link.href = data[i].html_url;
+        
         tableData.appendChild(link);
         createTableRow.appendChild(tableData);
-        tableBody.appendChild(createTableRow);
+        
   }
 });
-
-function displayUdot(cameras, cam2) {
-  document.getElementById("#cameras").innerHTML = `Cameras: ${cameras}`;
-  document.getElementById("cam2").innerHTML = `Cam2: ${cam2}`;
-  document.createElement("#cameras");
-  
-}
-
-function getData() {
-  let cameras = document.getElementById("cameras");
-}
-
-fetch(url)
-  .then((response) => {
-    if (!response.ok) throw new Error("Error")
-    return response.json();
-  })
-  .then((dataArray) => {
-
-    cameras.innerHTML = dataArray
-    .map(({cameras}) => {
-      return `<div>${cameras}</div>`;
-    }).join("");
-  })
-  .catch(console.warn);
-
-  displayUdot()
